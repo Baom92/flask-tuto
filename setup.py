79253ce2afedba0d0f4761eff5032e485cf8f909
rodolphe <rodyy007@gmail.com>
2020-03-09 14:23:18 +0000

    Purge of anithing but the tutorials

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import io
-import re
 
 from setuptools import find_packages
 from setuptools import setup
@@ -7,74 +6,18 @@ from setuptools import setup
 with io.open("README.rst", "rt", encoding="utf8") as f:
     readme = f.read()
 
-with io.open("src/flask/__init__.py", "rt", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
-
 setup(
-    name="Flask",
-    version=version,
-    url="https://palletsprojects.com/p/flask/",
-    project_urls={
-        "Documentation": "https://flask.palletsprojects.com/",
-        "Code": "https://github.com/pallets/flask",
-        "Issue tracker": "https://github.com/pallets/flask/issues",
-    },
-    license="BSD-3-Clause",
-    author="Armin Ronacher",
-    author_email="armin.ronacher@active-4.com",
-    maintainer="Pallets",
+    name="flaskr",
+    version="1.0.0",
+    url="https://flask.palletsprojects.com/tutorial/",
+    license="BSD",
+    maintainer="Pallets team",
     maintainer_email="contact@palletsprojects.com",
-    description="A simple framework for building complex web applications.",
+    description="The basic blog app built in the Flask tutorial.",
     long_description=readme,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Flask",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     include_package_data=True,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
-    install_requires=[
-        "Werkzeug>=0.15",
-        "Jinja2>=2.10.1",
-        "itsdangerous>=0.24",
-        "click>=5.1",
-    ],
-    extras_require={
-        "dotenv": ["python-dotenv"],
-        "dev": [
-            "pytest",
-            "coverage",
-            "tox",
-            "sphinx",
-            "pallets-sphinx-themes",
-            "sphinxcontrib-log-cabinet",
-            "sphinx-issues",
-        ],
-        "docs": [
-            "sphinx",
-            "pallets-sphinx-themes",
-            "sphinxcontrib-log-cabinet",
-            "sphinx-issues",
-        ],
-    },
-    entry_points={"console_scripts": ["flask = flask.cli:main"]},
+    zip_safe=False,
+    install_requires=["flask"],
+    extras_require={"test": ["pytest", "coverage"]},
 )
